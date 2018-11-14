765cf8758100cb82ae1a7a4be7e056341bfd74b0
Spazzlefrazzle <juuncaerlum@gmail.com>
2018-11-14 11:51:17 -0500

    Setting up previous room functionality.

--- src/drawing/DrawSpace.js
+++ src/drawing/DrawSpace.js
@@ -2,38 +2,91 @@ import React, {Component} from 'react';
 import { SketchPad, TOOL_PENCIL, TOOL_LINE, TOOL_RECTANGLE, TOOL_ELLIPSE }
   from 'react-sketchpad/lib';
 
-
 //Following the example here:
 //https://github.com/svrcekmichal/react-sketchpad/blob/master/example/SketchExample.jsx
 class DrawSpace extends Component {
   constructor(props) {
     super(props);
 
+    let tool:TOOL_PENCIL;
+    let size: 2;
+    let color: '#000000';
+    let fill: false;
+    let fillColor: '#444444';
+    let items = [];
+
     this.state = {
-      tool:TOOL_PENCIL,
-      size: 2,
-      color: '#000000',
-      fill: false,
-      fillColor: '#444444',
-      items: []
+      tool: tool,
+      size: size,
+      color: color,
+      fill: fill,
+      fillColor: fillColor,
+      rooms: [
+        <SketchPad
+          class="drawing-area"
+          width={500}
+          height={500}
+          animate={true}
+          size={size}
+          color={color}
+          fillColor={fill ? fillColor : ''}
+          items={items}
+          tool={tool}
+        />
+      ]
     }
+
+    this.resetSketchpad = this.resetSketchpad.bind(this);
+    this.getCurrentRoom = this.getCurrentRoom.bind(this);
+    this.getPreviousRooms = this.getPreviousRooms.bind(this);
+  }
+
+  resetSketchpad() {
+    let blankPad =
+      <SketchPad
+        class="drawing-area"
+        width={500}
+        height={500}
+        animate={true}
+        size={this.state.size}
+        color={this.state.color}
+        fillColor={this.state.fill ? this.state.fillColor : ''}
+        items={[]}
+        tool={this.state.tool}
+      />
+      this.setState(prevState =>
+        ({
+          rooms: [...prevState.rooms, blankPad]
+        })
+      )
+  }
+
+  getCurrentRoom() {
+    //return this.state.rooms[this.state.rooms.length - 1]
+    return this.state.rooms[0];
+  }
+
+  getPreviousRooms() {
+    let rooms = [];
+    for(let i=0; i<this.state.rooms.length - 1; i++) {
+      rooms.push(this.state.rooms[i])
+    }
+    return rooms;
   }
 
   render() {
     return (
       <div>
         <p>Your dungeon, here.</p>
-        <SketchPad
-          class="drawing-area"
-          width={500}
-          height={500}
-          animate={true}
-          size={this.state.size}
-          color={this.state.color}
-          fillColor={this.state.fill ? this.state.fillColor : ''}
-          items={this.state.items}
-          tool={this.state.tool}
-        />
+        <div>
+          <button onClick={this.resetSketchpad}>Save</button>
+        </div>
+        <div>
+          {this.getCurrentRoom()}
+        </div>
+        <div>
+          {this.getPreviousRooms()}
+        </div>
       </div>
     )
   }
